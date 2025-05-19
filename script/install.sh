5b00878d7879e7d908de6a6a87d186810014dbe6
Abhishekc164 <chimanpure164@gmail.com>
2025-05-19 07:37:09 +0530

    updating files

---
+++ script/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo systemctl stop nginx
+echo "Clearing old build files..."
+sudo rm -rf /usr/share/nginx/html/*
+
+echo "Copying new build files to Nginx directory..."
+sudo cp -r /home/ec2-user/nodejsbuild/* /usr/share/nginx/html/
+
+#.
+echo "Restarting Nginx..."
+sudo systemctl start nginx
+sudo systemctl enable nginx
